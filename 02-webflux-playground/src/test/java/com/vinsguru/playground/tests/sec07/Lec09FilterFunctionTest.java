fa43e68d8abd8386f325a1c7bfe94dd23d0f6968
dorotajachtoma <jachtoma.do@gmail.com>
2026-08-05 09:51:48 +0200

    7# non-blocking webclient

--- 02-webflux-playground/src/test/java/com/vinsguru/playground/tests/sec07/Lec09FilterFunctionTest.java
+++ 02-webflux-playground/src/test/java/com/vinsguru/playground/tests/sec07/Lec09FilterFunctionTest.java
@@ -2,13 +2,16 @@ package com.vinsguru.playground.tests.sec07;
 
 import com.vinsguru.playground.tests.sec07.dto.Product;
 import org.junit.jupiter.api.Test;
+import org.springframework.web.reactive.function.client.ClientRequest;
+import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.test.StepVerifier;
 
+import java.util.UUID;
+
 public class Lec09FilterFunctionTest extends AbstractWebClient {
 
-    private final WebClient webClient = this.createWebClient(builder ->
-            builder.defaultHeaders(s -> s.setBearerAuth("auth")));
+    private final WebClient webClient = this.createWebClient(b -> b.filter(tokenGeneration()));
 
     @Test
     public void filterFunctionTest() {
@@ -21,5 +24,13 @@ public class Lec09FilterFunctionTest extends AbstractWebClient {
                 .as(StepVerifier::create)
                 .expectComplete()
                 .verify();
+    }
+
+    private ExchangeFilterFunction tokenGeneration() {
+        return (request, next) -> {
+            var token = UUID.randomUUID().toString().replace("-", "");
+            var modified = ClientRequest.from(request).headers(h -> h.setBearerAuth(token)).build();
+            return next.exchange(modified);
+        };
     }
 }
