7547fc9e124c66c3e4cb8345bfd27d0ec9ba6f16
dorotajachtoma <jachtoma.do@gmail.com>
2026-08-05 09:27:02 +0200

    7# non-blocking webclient

--- 02-webflux-playground/src/test/java/com/vinsguru/playground/tests/sec07/Lec05ErrorHandlingTest.java
+++ 02-webflux-playground/src/test/java/com/vinsguru/playground/tests/sec07/Lec05ErrorHandlingTest.java
@@ -1,5 +1,6 @@
 package com.vinsguru.playground.tests.sec07;
 
+import com.vinsguru.playground.tests.sec07.dto.CalculatorResponse;
 import com.vinsguru.playground.tests.sec07.dto.Product;
 import org.junit.jupiter.api.Test;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -11,12 +12,27 @@ public class Lec05ErrorHandlingTest extends AbstractWebClient {
 
 
     @Test
-    public void defaultHeaderTest() {
+    public void errorHandlingHappyPathTest() {
         webClient.get()
-                .uri("/lec05/product/{id}", 1)
+                .uri("/lec05/calculator/{first}/{second}", 10, 20)
+                .header("operation", "+")
                 .retrieve()
-                .bodyToMono(Product.class)
+                .bodyToMono(CalculatorResponse.class)
                 .doOnError(print())
+                .then()
+                .as(StepVerifier::create)
+                .expectComplete()
+                .verify();
+    }
+
+    @Test
+    public void errorHandlingNegativePathTest() {
+        webClient.get()
+                .uri("/lec06/calculator/{first}/{second}", 10, 20)
+                .retrieve()
+                .bodyToMono(CalculatorResponse.class)
+                .doOnError(print())
+                .onErrorReturn(new CalculatorResponse(0, 0, null, 0.0))
                 .then()
                 .as(StepVerifier::create)
                 .expectComplete()
