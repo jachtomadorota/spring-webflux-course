1c8e136749a02923da8e76a11991b028c09a6fc2
dorotajachtoma <jachtoma.do@gmail.com>
2026-08-05 09:16:18 +0200

    7# non-blocking webclient

--- 02-webflux-playground/src/test/java/com/vinsguru/playground/tests/sec07/Lec03PostTest.java
+++ 02-webflux-playground/src/test/java/com/vinsguru/playground/tests/sec07/Lec03PostTest.java
@@ -4,6 +4,7 @@ import com.vinsguru.playground.tests.sec07.dto.Product;
 import org.junit.jupiter.api.Test;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
 
 public class Lec03PostTest extends AbstractWebClient {
 
@@ -11,13 +12,30 @@ public class Lec03PostTest extends AbstractWebClient {
 
 
     @Test
-    public void postTest() {
+    public void postTestBodyValue() {
         webClient.post()
-                .uri("/lec02/product")
-                //.body(Mono.just(new Product(11, "Product description", 340)))
+                .uri("/lec03/product")
+                .bodyValue(new Product(11, "Product description", 340))
                 .retrieve()
                 .bodyToMono(Product.class)
                 .doOnError(print())
-                .subscribe();
+                .then()
+                .as(StepVerifier::create)
+                .expectComplete()
+                .verify();
+    }
+
+    @Test
+    public void postTestBody() {
+        webClient.post()
+                .uri("/lec03/product")
+                .body(Mono.just(new Product(11, "Product description", 340)), Product.class)
+                .retrieve()
+                .bodyToMono(Product.class)
+                .doOnError(print())
+                .then()
+                .as(StepVerifier::create)
+                .expectComplete()
+                .verify();
     }
 }
