a5ed045abb05e5295fed294f16cd199660057b20
dorotajachtoma <jachtoma.do@gmail.com>
2026-08-10 16:16:07 +0200

    #8 gzip and connections pooling

--- 02-webflux-playground/src/test/java/com/vinsguru/playground/tests/sec10/Lec01HttpConnectionPoolingTest.java
+++ 02-webflux-playground/src/test/java/com/vinsguru/playground/tests/sec10/Lec01HttpConnectionPoolingTest.java
@@ -3,14 +3,28 @@ package com.vinsguru.playground.tests.sec10;
 import com.vinsguru.playground.tests.sec10.dto.Product;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.netty.http.client.HttpClient;
+import reactor.netty.resources.ConnectionProvider;
 import reactor.test.StepVerifier;
 
 public class Lec01HttpConnectionPoolingTest extends AbstractWebClient {
 
-    private final WebClient client = this.createWebClient();
+    private final WebClient client = this.createWebClient(b -> {
+        var poolSize = 500;
+        var provider = ConnectionProvider.builder("webClientTest")
+                .lifo()
+                .maxConnections(poolSize)
+                .pendingAcquireMaxCount(poolSize * 5)
+                .build();
+        var httpClient = HttpClient.create(provider)
+                .compress(true)
+                .keepAlive(true);
+        b.clientConnector(new ReactorClientHttpConnector(httpClient));
+    });
 
     @Test
     public void concurrentRequestTest() {
@@ -24,7 +38,7 @@ public class Lec01HttpConnectionPoolingTest extends AbstractWebClient {
                 .verify();
     }
 
-    private Mono<Product> getProduct(int i ) {
+    private Mono<Product> getProduct(int i) {
         return this.client.get()
                 .uri("/product/{id}", i)
                 .retrieve().bodyToMono(Product.class);
